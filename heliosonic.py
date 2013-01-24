734c63454af1f1ec7b5a0ba815d5ae13599d4a4e
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2013-01-23 18:53:09 -0700

    added doc strings, reorganized

--- heliosonic.py
+++ heliosonic.py
@@ -1,5 +1,11 @@
-import pylab
-import pyfits
+"""
+heliosonic.py
+Tools for the sonficiation of helioseismic data.
+Chris Chronopoulos, 01-22-2013
+chronopoulos.chris@gmail.com
+"""
+
+import os, pylab, pyfits
 import numpy as np
 import scikits.audiolab as audiolab
 
@@ -26,8 +32,13 @@ def apodMask(nx,ny):
    mask[indOuter] = 0.
    return mask
 
+###
 
 class imgObject():
+   """
+   Encapsulates the raw data (image, as opposed to its transform) and its manipulations.
+   Takes an SDO/HMI FITS file as an argument.
+   """
 
    def __init__(self,filename):
       print 'Reading in file '+filename
@@ -35,15 +46,30 @@ class imgObject():
       self.data = fitsFile[0].data
       self.nt, self.ny, self.nx = pylab.shape(self.data)
 
-   def computeFFT(self,filename):
+   def computeFFT(self,*arg):
+      """
+      Compute the spatial FFT of each timestep of the data set.
+      Uses a tapered-tophat 2D apodization mask.
+      If called without an argument, return the result.
+      If called with a string argument ending in .npy, then save to that file in NumPy format.
+      """
       spatialMask=apodMask(self.ny,self.nx)
       data_fft = pylab.zeros( (self.nt,self.ny,self.nx), dtype='complex64' )
       for i in range(self.nt):
          print 'Timestep '+str(i)+' of '+str(self.nt)
          data_fft[i,:,:] = np.fft.fft2(spatialMask*self.data[i,:,:])
-      np.save(filename, data_fft[:,0:nx/2-1,0:nx/2-1])
+      if len(arg)==0:
+         return data_fft
+      elif os.path.splitext(arg)[1]=='.py':
+         np.save(arg, data_fft[:,0:nx/2-1,0:nx/2-1])
+      else:
+         raise Exception('Invalid filename argument')
 
-   def makeMovie(self,filename,imgDir='/home/chrono/data/blork/helio/img/'):
+   def makeMovie(self,imgDir=''):
+      """
+      Write out 2D colormaps of each timestep, to be used as frames in a movie.
+      Optional argument imgDir specifies the directory in which to place the files.
+      """
       for i in range(self.nt):
          imgPlot = pylab.imshow(self.data[i,:,:])
          imgPlot.set_cmap('hot')
@@ -54,6 +80,10 @@ class imgObject():
       
 
 class fftObject():
+   """
+   Encapsulates the spatial Fourier Transform of the data, and its manipulations.
+   Takes a NumPy .npy file containing the FFT data as the argument.
+   """
 
    def __init__(self,filename):
       print 'Reading in file '+filename
@@ -63,7 +93,13 @@ class fftObject():
       self.kr = np.sqrt(self.kx**2 + self.ky**2)
       self.ktheta = np.arctan(self.ky/self.kx)
 
-   def plotSignalSpectrum(self,signal,spectrum):
+   def plotSignalSpectrum(self,signal):
+      """
+      Display a plot of the signal and its spectrum.
+      Argument: 1D signal array
+      """
+      fft = np.fft.fft(signal)
+      spectrum = np.absolute(fft[0:self.nt/2-1])**2
       pylab.figure(figsize=(14,6))
       pylab.subplot(121, axisbg='k')
       pylab.plot(signal,'y-')
@@ -73,29 +109,23 @@ class fftObject():
       pylab.title('Spectrum')
       pylab.show()
 
-   def displaySingleMode(self,ikx,iky):
+   def displayMode(self,ikx,iky):
+      """
+      Display the signal and spectrum of a single mode (kx,ky).
+      Arguments: indeces ikx, iky
+      """
       signal=self.data[:,iky,ikx]
-      fft = np.fft.fft(signal)
-      spectrum = np.absolute(fft[0:self.nt/2-1])**2
-      self.plotSignalSpectrum(signal,spectrum)
+      self.plotSignalSpectrum(signal)
 
-   def displaySingleKr(self,ikr,dikr=2):
+   def displayRing(self,ikr,dikr=2):
+      """
+      Display the signal and spectrum of all modes in a ring of radius kr.
+      Arguments: index ikr, ring width dikr (optional)
+      """
       indRing = np.where( (self.kr >= ikr-dikr)*(self.kr <= ikr+dikr) )
       signal=np.zeros(self.nt)
       for i in range(np.shape(indRing)[1]):
          signal += self.data[ :, indRing[1][i], indRing[0][i] ]
-      fft = np.fft.fft(signal)
-      spectrum = np.absolute(fft[0:self.nt/2-1])**2
-      self.plotSignalSpectrum(signal,spectrum)
+      self.plotSignalSpectrum(signal)
 
 
-# Main Program
-if __name__=='__main__':
-   tile16_img=imgObject('/home/chrono/data/blork/helio/tile16_+285.0000_+0.0000.fits')
-   (nt,ny,nx) = pylab.shape(data_img)
-   spatialMask=apodMask(ny,nx)
-   data_fft = pylab.zeros( (nt,ny,nx), dtype='complex64' )
-   for i in range(nt):
-      print 'Timestep '+str(i)+' of '+str(nt)
-      data_fft[i,:,:] = pylab.fft2(spatialMask*data_img[i,:,:])
-   np.save('/home/chrono/data/blork/helio/data_fft.npy', data_fft[:,0:nx/2-1,0:nx/2-1])
